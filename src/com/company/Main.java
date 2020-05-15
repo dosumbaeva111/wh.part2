ed7c5d76da4c4024369f8b21299c6f55e71de210
dosumbaeva111 <dosumbaeva111@gmail.com>
2020-05-16 04:05:37 +0600

    Изменен

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,14 @@
 package com.company;
 
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Semaphore;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class Main {
+    private static final int DOWLOADERS_LIMIT = 3;
+    private static final int DOWLOADS_LIMIT = 10;
+
     //a)	Написать многопоточное приложение которое бы симулировало загрузку файлов на сервер. И их скачивание.
     // Используя классы Semaphore и CountDownLatch. Либо можно использовать методы wait(), notify(), notifyAll().
     //b)	Uploader загружает  1 файл 500 мб на сервер. Скорость загрузки 20 мб в секунду.
@@ -18,14 +22,41 @@ public class Main {
         uploader.start();
         try {
             uploader.join();
-            new Downloader(new Semaphore(3, true), new CountDownLatch(10), 500, 100).start();
-            for (int i = 1; i <= 10; i++) {
-                if (i == 10) {
-                    System.out.println(" Файл удален!!! ");
-                }
+            /**
+             * Создаем сервис для контроля количества запущенных потоков
+             * и отдаем в конструктор лимит одновременно выполняемых задач
+             */
+            ExecutorService serivce = Executors.newFixedThreadPool(DOWLOADERS_LIMIT);
+            /**
+             * Создаем лист для хранения всех задач, которые нужно выполнить
+             */
+            List<Future<Downloader>> downloaders = new ArrayList<>();
+            for (int i = 1; i <= DOWLOADS_LIMIT; i++) {
+                /**
+                 * Внутри создаем экземпляр загрузчика.
+                 * Создаем обьект асинхронного менеджера и отдаем ему задачу(поток)
+                 * затем добавляем менеджера с задачей в лист
+                 */
+                Downloader downloader = new Downloader(new Semaphore(3, true), new CountDownLatch(10), 500, 100);
+                Future d = serivce.submit(downloader);
+                downloaders.add(d);
 
             }
-        } catch (InterruptedException e) {
+            /**
+             * Пробегаемся по листу с менеджерами асинхронных задач
+             * и начинаем их инициализацию и выполнение
+             */
+            for (Future<Downloader> d : downloaders) {
+                d.get();
+            }
+            /**
+             * Завершаем все потоки.
+             */
+            serivce.shutdown();
+
+            System.out.println(" Файл удален!!! ");
+
+        } catch (InterruptedException | ExecutionException e) {
 
         }
 
